d8d7881acd2163c0c819804db60f3f0bc6e6c779
adelmojsantos <adelmojsantos1985@gmail.com>
2024-09-06 20:32:18 -0300

    Atualizando aplicação adicionando imagem e texto para quando a pesquisa não retorna dados ou não se pesquisou nada ainda

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 function pesquisar() {
+  let mensagemInicial = document.getElementById('mensagem-inicial')
   let campoPesquisa = document.getElementById('campo-pesquisa').value
   if (!campoPesquisa) {
-    document.getElementById('resultados-pesquisa').innerHTML = `
-    <div class="item-resultado">
-    <p>Nada foi encontrado. Informe um tipo de cerveja ou a harmonização desejada.</p>
-    <div>
-    `
-    return
+    mensagemInicial.classList.remove('ocultar')
   }
   let resultado = ""
-  for (let cerveja of cervejas) {
-    if (
-      cerveja.titulo.toLowerCase().includes(campoPesquisa.toLowerCase()) || cerveja.descricao.toLowerCase().includes(campoPesquisa.toLowerCase()) ||
-      cerveja?.tags?.toLowerCase().includes(campoPesquisa?.toLowerCase())
-    ) {
-      resultado += `
+  if (campoPesquisa) {
+    for (let cerveja of cervejas) {
+      if (
+        cerveja.titulo.toLowerCase().includes(campoPesquisa.toLowerCase()) || cerveja.descricao.toLowerCase().includes(campoPesquisa.toLowerCase()) ||
+        cerveja?.tags?.toLowerCase().includes(campoPesquisa?.toLowerCase())
+      ) {
+        resultado += `
         <div class="item-resultado">
           <h2>
             ${cerveja.titulo}
           </h2>
           <p class="descricao-meta">${cerveja.descricao}</p>
           <h3>
-            <a href="#">Harmonizações</a>
+           Harmonizações
           </h3>
           <ul>
             <li><strong>Carnes:</strong> ${cerveja.harmonizacoes.carnes}</li>
@@ -32,15 +29,19 @@ function pesquisar() {
           <a href=${cerveja.link} target="_blank">Saiba mais</a>
         </div>
     `
+      }
     }
   }
   if (!resultado) {
+    mensagemInicial.classList.remove('ocultar')
     resultado = `
-      <div class="item-resultado">
+      <div class="sem-resultado">
         <p>Nada foi encontrado</p>
       <div>
   `
+  } else {
+    mensagemInicial.classList.add('ocultar')
   }
-
   document.getElementById('resultados-pesquisa').innerHTML = resultado
+
 }
